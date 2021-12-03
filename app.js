4911380f7e295f28959109a12feb11db478d16bc
badger <badger@badger.local>
2021-12-03 11:13:26 +0200

    Section 7. Starting code

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
-const http = require('http');
-const routes = require('./routes');
+const express = require('express');
+const bodyParser = require('body-parser')
+const path = require('path')
 
-const server = http.createServer(routes);
+const rootDir = require('./util/path')
 
-server.listen(3000);
+const adminData = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+	res
+		.status(404)
+		.sendFile(path.join(rootDir, 'views', '404.html'));
+})
+
+app.listen(3000);
