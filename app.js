9b111250742a04a1546f882b7969e813659993fb
badger <badger@badger.local>
2021-12-04 10:51:51 +0200

    108. Working on the Navigation

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const path = require('path')
 
-const rootDir = require('./util/path')
+const errorController = require('./controllers/error');
 
-const adminData = require('./routes/admin')
+const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 const app = express();
@@ -16,13 +16,9 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-	res
-		.status(404)
-		.sendFile(path.join(rootDir, 'views', '404.html'));
-})
+app.use(errorController.get404)
 
 app.listen(3000);
---
+++ controllers/error.js
@@ -0,0 +1,7 @@
+const path = require('path')
+
+exports.get404 = (req, res, next) => {
+	res
+		.status(404)
+		.sendFile(path.join(rootDir, 'views', '404.html'));
+}
